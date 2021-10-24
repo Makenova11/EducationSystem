412980237243ea6a227bd1dc59a4fdb653b58ad9
Галина Макенова <makenova11@mail.ru>
2021-10-25 00:43:51 +0700

    Добавьте файлы проекта.

---
+++ EducationSystem/Controllers/ExpertsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EducationSystem.Models;
+
+namespace EducationSystem.Controllers
+{
+    public class ExpertsController : Controller
+    {
+        private EducationSystemDB db = new EducationSystemDB();
+        // GET: Experts
+        public ActionResult Index()
+        {
+            return View(db.Expert.ToList());
+        }
+    }
+}
